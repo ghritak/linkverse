1d0c912729faf879bcd1ee4356ecc14e01b2fcee
ghritak <ghritakjyotikalita@gmail.com>
2024-03-17 01:50:31 +0530

    first commit

---
+++ pages/[username].js
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const UserProfile = ({ userData }) => {
+  const router = useRouter();
+  const { user } = router.query;
+
+  return (
+    <div>
+      <h1>User Profile: {user}</h1>
+      <pre>{JSON.stringify(userData, null, 2)}</pre>
+    </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { username } = params;
+  const URL = process.env.NEXT_PUBLIC_API_URL;
+
+  const res = await fetch(`${URL}/api/link?username=${username}`);
+  const userData = await res.json();
+
+  return {
+    props: {
+      userData,
+    },
+  };
+}
+
+export default UserProfile;
