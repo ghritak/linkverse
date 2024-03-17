8cb965a52beaa8d088d7d7f9defd81b5d8e3e0ab
ghritak <ghritakjyotikalita@gmail.com>
2024-03-17 19:45:44 +0530

    logout

--- components/button/Button.js
+++ components/button/Button.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Button = ({ props, children, disabled }) => {
+const Button = ({ props, children, disabled, className }) => {
   return (
-    <div>
-      <button
-        disabled={disabled}
-        className={`bg-blue-500 text-white py-2 px-6 rounded-lg  transition-all duration-300 ${
-          disabled ? 'opacity-50' : 'hover:bg-blue-600 '
-        }`}
-        {...props}
-      >
-        {children}
-      </button>
-    </div>
+    <button
+      disabled={disabled}
+      className={`bg-blue-500 text-white py-2 px-6 rounded-lg transition-all duration-300 ${
+        disabled ? 'opacity-50' : 'hover:bg-blue-600 '
+      } ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
   );
 };
 
--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { IoShareOutline } from 'react-icons/io5';
 import { FiEdit, FiLogOut } from 'react-icons/fi';
 import { BsThreeDotsVertical } from 'react-icons/bs';
+import Button from '../../components/button/Button';
 
 const page = () => {
   const router = useRouter();
@@ -14,27 +15,7 @@ const page = () => {
   const [menuVisible, setMenuVisible] = useState(false);
   const menuRef = useRef(null);
   const [isEditMode, setEditMode] = useState(false);
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-  const handleClickOutside = (event) => {
-    if (
-      menuRef.current &&
-      !menuRef.current.contains(event.target) &&
-      event.target.id !== 'menuButton'
-    ) {
-      setMenuVisible(false);
-    }
-  };
-
-  const handleButtonClick = (event) => {
-    setMenuVisible(!menuVisible);
-  };
+  const [links, setLinks] = useState([]);
 
   useEffect(() => {
     const user = localStorage.getItem('USER');
@@ -59,8 +40,30 @@ const page = () => {
     }
   };
 
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      event.target.id !== 'menuButton'
+    ) {
+      setMenuVisible(false);
+    }
+  };
+
+  const handleButtonClick = (event) => {
+    setMenuVisible(!menuVisible);
+  };
+
   const handleExternalLinkClick = (event, link) => {
     event.preventDefault();
+    if (isEditMode) return;
     window.open(link, '_blank');
   };
 
@@ -77,26 +80,41 @@ const page = () => {
 
   const handleLogout = () => {
     setMenuVisible(false);
+    const confirmed = window.confirm('Are you sure you want to logout ?');
+    if (confirmed) {
+      localStorage.clear();
+      router.push('/login');
+      console.log('User confirmed logout');
+    }
   };
 
   return (
     <div className='bg-gradient-to-tr from-gray-500 via-gray-700 to-black w-screen  h-screen flex justify-center overflow-hidden'>
       {!loading ? (
-        <div className='relative max-w-3xl  md:min-w-[700px] md:bg-gradient-to-tr from-gray-500 via-gray-700 to-black overflow-y-scroll px-10 sm:px-20'>
-          <button
-            onClick={handleButtonClick}
-            aria-expanded={menuVisible}
-            aria-haspopup='true'
-            id='menuButton'
-            className='absolute top-6 right-6 cursor-pointer hover:scale-95 hover:bg-white hover:text-black flex items-center justify-center text-white rounded-full p-2 transition-all duration-300'
-          >
-            <span id='menuButton'>
-              <BsThreeDotsVertical id='menuButton' />
-            </span>
-          </button>
+        <div className='relative max-w-3xl  md:min-w-[700px] md:bg-gradient-to-tr from-gray-500 via-gray-700 to-black overflow-y-scroll w-full px-8 md:px-20'>
+          <div className='absolute top-6 right-8 md:right-20'>
+            {!isEditMode ? (
+              <button
+                onClick={handleButtonClick}
+                id='menuButton'
+                className=' cursor-pointer hover:scale-95 bg-white hover:text-black flex items-center justify-center text-white rounded-full p-2 transition-all duration-300'
+              >
+                <span id='menuButton'>
+                  <BsThreeDotsVertical id='menuButton' color='black' />
+                </span>
+              </button>
+            ) : (
+              <button
+                onClick={() => setEditMode(false)}
+                className='bg-white hover:text-black rounded-2xl hover:bg-gray-200 transition-all duration-300 px-4 py-1'
+              >
+                Cancel
+              </button>
+            )}
+          </div>
           <div
             ref={menuRef}
-            className={`absolute top-16 right-10 text-black transition-opacity duration-200 bg-white rounded-md ${
+            className={`absolute top-16 right-10 md:right-24 text-black transition-opacity duration-200 bg-white rounded-md z-20 ${
               menuVisible ? '' : 'opacity-0 pointer-events-none'
             }`}
           >
@@ -144,7 +162,9 @@ const page = () => {
                   <div
                     key={index}
                     onClick={(e) => handleExternalLinkClick(e, item.link)}
-                    className='flex justify-between items-center my-6 w-full flex-1 border-[1px] p-2 rounded-lg cursor-pointer text-white bg-gray-600 hover:scale-[102%] transition-all duration-300'
+                    className={`flex justify-between items-center my-6 w-full flex-1 border-[1px] p-2 rounded-lg text-white bg-gray-600 ${
+                      isEditMode ? '' : 'hover:scale-[102%] cursor-pointer'
+                    } transition-all duration-300`}
                   >
                     {item.logo ? (
                       <Image
@@ -158,18 +178,31 @@ const page = () => {
                       <div className='w-10 h-10 bg-gray-300 animate-pulse rounded-lg'></div>
                     )}
                     <div className='md:min-w-[200px]'>
-                      <h1 className='text-center px-2'>{item?.name}</h1>
+                      {/* <h1 className='text-center px-2'>{item?.name}</h1> */}
+                      <input
+                        value={item?.name}
+                        className='bg-gray-600 text-center outline-none'
+                      />
                     </div>
-                    <div
-                      onClick={(e) => handleClickDot(e)}
-                      className='hover:scale-125 transition-all duration-300 text-lg mr-3'
-                    >
-                      <IoShareOutline size={24} />
-                    </div>
+                    {!isEditMode ? (
+                      <div
+                        onClick={(e) => handleClickDot(e)}
+                        className='hover:scale-125 transition-all duration-300 text-lg mr-3'
+                      >
+                        <IoShareOutline size={24} />
+                      </div>
+                    ) : (
+                      <div className='w-[24px] mr-3' />
+                    )}
                   </div>
                 );
               })}
           </div>
+          {isEditMode && (
+            <div className='absolute flex-1 w-full bottom-10 left-0 px-10 sm:px-20'>
+              <Button className={'w-full py-3'}>Save</Button>
+            </div>
+          )}
         </div>
       ) : (
         <div className='h-screen flex items-center justify-center text-white'>
