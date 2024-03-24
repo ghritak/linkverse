61d1e4580e13bb7996e3dac513fba9ebe3e5c38e
ghritak <ghritakjyotikalita@gmail.com>
2024-03-24 20:46:51 +0530

    c

--- pages/learnmore.js
+++ pages/learnmore.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
-function LearnMorePage() {
+const LearnMorePage = () => {
   const router = useRouter()
   return (
     <div className="bg-gray-100 min-h-screen p-10">
